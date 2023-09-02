ec426f1b2db585c9c2a56e3c221cb2227ba9d0b0
Dominik Lamcha <lamcha.dominik@gmail.com>
2023-09-02 18:07:54 +0200

    Enigma without printouts

--- src/Enigma.cpp
+++ src/Enigma.cpp
@@ -5,13 +5,9 @@ Enigma::Enigma(Rotor rotor, Reflector reflector) : rotor(rotor), reflector(refle
 
 char Enigma::encrypt(char in) {
     
-    std::cout << in <<std::endl;
     char r = rotor.forward(in);
-    std::cout << r <<std::endl;
     char ref = reflector.reflect(r);
-    std::cout << ref <<std::endl;
     char out = rotor.reverse(ref);
-    std::cout << out <<std::endl;
     rotor.rotate();
     return out;
 }
