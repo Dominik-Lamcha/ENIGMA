cd54c97a729d6758704e755bf2481ac2e750f680
Dominik Lamcha <lamcha.dominik@gmail.com>
2023-09-02 16:57:25 +0200

    reflector configurator selector

--- include/Reflector.hpp
+++ include/Reflector.hpp
@@ -4,13 +4,16 @@
 #include <unordered_map>
 #include <string>
 #include <stdexcept>
+#include <vector>
 
 class Reflector {
 private:
     std::unordered_map<char, char> mappings;
+    void setByNumber(int reflectorNumber);
 
 public:
     Reflector(const std::string& settings);
+    Reflector(int rotorNumber); 
     char reflect(char input) const;
 };
 
--- src/Reflector.cpp
+++ src/Reflector.cpp
@@ -13,6 +13,25 @@ Reflector::Reflector(const std::string& settings) {
     }
 }
 
+Reflector::Reflector(int reflectorNumber) {
+    setByNumber(reflectorNumber);
+}
+
+void Reflector::setByNumber(int reflectorNumber) {
+    static const std::vector<std::string> reflectorConfigs = {
+        "EJMZALYXVBWFCRQUONTSPIKHGD",  // I
+        "YRUHQSLDPXNGOKMIEBFZCWVJAT",  // II
+        "FVPJIAOYEDRZXWGCTKUQSBNMHL",  // III
+    };
+
+    if (reflectorNumber < 1 || reflectorNumber > 3) {
+        throw std::invalid_argument("Invalid reflector number. Must be between 1 and 3.");
+    }
+
+    // Initialize reflector settings based on rotor number
+    *this = Reflector(reflectorConfigs[reflectorNumber - 1]);
+}
+
 char Reflector::reflect(char input) const {
     auto it = mappings.find(input);
     if (it != mappings.end()) {
