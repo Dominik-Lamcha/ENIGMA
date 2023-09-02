75ec31bdac51862a72256711cb15d501c7851d63
Dominik Lamcha <lamcha.dominik@gmail.com>
2023-09-02 18:41:45 +0200

    notch constructors in Rotor class

--- include/Rotor.hpp
+++ include/Rotor.hpp
@@ -12,14 +12,19 @@ private:
     std::unordered_map<char, char> reverseMappings;
     int offset;
     void setByNumber(int rotorNumber);
+    char first_notch;
+    char second_notch;
 
 public:
     Rotor(const std::string& settings);
+    Rotor(const std::string& settings, char first_notch);
+    Rotor(const std::string& settings, char first_notch, char second_notch);
     Rotor(int rotorNumber); 
     void setOffset(int newOffset);
     char forward(char input) const;
     char reverse(char input) const;
     void rotate();
+    bool atNotch();
 };
 
 #endif // ROTOR_HPP
--- src/Rotor.cpp
+++ src/Rotor.cpp
@@ -13,6 +13,36 @@ Rotor::Rotor(const std::string& settings) : offset(0) {
     }
 }
 
+Rotor::Rotor(const std::string& settings, char first_notch) : offset(0) {
+    if (settings.length() != 26) {
+        throw std::runtime_error("Invalid settings length. Should be 26 characters.");
+    }
+
+    for (size_t i = 0; i < settings.length(); ++i) {
+        char from = 'A' + i;
+        char to = settings[i];
+        forwardMappings[from] = to;
+        reverseMappings[to] = from;
+    }
+    this->first_notch = first_notch;
+    this->second_notch = first_notch;
+}
+
+Rotor::Rotor(const std::string& settings, char first_notch, char second_notch) : offset(0) {
+    if (settings.length() != 26) {
+        throw std::runtime_error("Invalid settings length. Should be 26 characters.");
+    }
+
+    for (size_t i = 0; i < settings.length(); ++i) {
+        char from = 'A' + i;
+        char to = settings[i];
+        forwardMappings[from] = to;
+        reverseMappings[to] = from;
+    }
+    this->first_notch = first_notch;
+    this->second_notch = second_notch;
+}
+
 Rotor::Rotor(int rotorNumber) {
     setByNumber(rotorNumber);
 }
@@ -28,13 +58,35 @@ void Rotor::setByNumber(int rotorNumber) {
         "NZJHGRCXMYSWBOUFAIVLPEKQDT",  // VII
         "FKQHTLXOCBJSPDZRAMEWNIUYGV"   // VIII
     };
+    static const std::vector<char> notchConfigs = {
+        'Y',  //I
+        'M',  //II
+        'D',  //III
+        'R',  //IV
+        'H'   //V
+        
+    };
 
     if (rotorNumber < 1 || rotorNumber > 8) {
         throw std::invalid_argument("Invalid rotor number. Must be between 1 and 5.");
     }
 
     // Initialize rotor settings based on rotor number
-    *this = Rotor(rotorConfigs[rotorNumber - 1]);
+
+    char first_notch;
+    char second_notch;
+
+    if (rotorNumber < 1 || rotorNumber > 5)
+    {
+        first_notch = notchConfigs[rotorNumber-1];
+        second_notch = first_notch;
+    }
+    else
+    {
+        first_notch = 'H';
+        second_notch = 'U';
+    }
+    *this = Rotor(rotorConfigs[rotorNumber - 1],first_notch,second_notch);
 }
 
 void Rotor::setOffset(int newOffset) {
